cfedcfa26a796807c459d98373072ed50a5cf2e4
0Jahid <iamjhashik@hotmail.com>
2023-06-05 16:37:24 +0600

    solved onclicklisterner function problem

--- app/src/main/java/com/example/healthmate/LoginActivity.java
+++ app/src/main/java/com/example/healthmate/LoginActivity.java
@@ -3,12 +3,37 @@ package com.example.healthmate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    //Declaring all variables for login activity
+    EditText edUsername, edPassword;
+    Button btn;
+    TextView tvRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        edUsername = findViewById(R.id.editTextUsername);
+        edPassword = findViewById(R.id.editTextLoginPassword);
+        btn = findViewById(R.id.buttonLogin);
+        tvRegister = findViewById(R.id.textViewNewUser);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                if (username.length() == 0 || password.length() == 0) {
+                    Toast.makeText(LoginActivity.this, "Please enter all fields", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
